569aede2535e4e9c8baf0bb56a3364421f3a48ff
sim-mokomo <15795655+sim-mokomo@users.noreply.github.com>
2021-05-09 11:40:08 +0900

    impl: 空のフレームレスウィンドウ表示できるように

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
-import { BrowserWindow } from "electron"
+import {BrowserView, BrowserWindow} from "electron"
 import { app } from "electron"
 
 void app.whenReady().then(() => {
-	const rootWindow = new BrowserWindow({})
+	const window = new BrowserWindow({ titleBarStyle: "hiddenInset"})
 
-	void rootWindow.loadFile("src/index.html")
+	void window.loadFile("src/index.html")
 
 	app.on("window-all-closed", () => {
 		app.quit()
