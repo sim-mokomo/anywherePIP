d5622fbd12b127af8ff9a00a4e282fcf02a0f42f
sim-mokomo <15795655+sim-mokomo@users.noreply.github.com>
2021-05-16 01:22:58 +0900

    impl: Chrome allways on top (test)

--- anywherePIP/Main.cpp
+++ anywherePIP/Main.cpp
@@ -3,8 +3,53 @@
 #endif 
 
 #include <windows.h>
+#include <algorithm>
+#include <stdio.h>
+#include <string.h>
+#include <vector>
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+BOOL CALLBACK EnumWindowsProc(HWND hwnd, LPARAM lp);
+void outputToConsole(const wchar_t* text);
+
+std::vector<HWND> hwnds;
+
+BOOL CALLBACK EnumWindowsProc(HWND hwnd, LPARAM lp)
+{
+    WCHAR windowTitle[256] = { };
+    auto size = sizeof(windowTitle) / sizeof(windowTitle[0]);
+    GetWindowText(hwnd,  windowTitle, size);
+    if (windowTitle[0] == 0) return TRUE;
+
+    auto findP = wcsstr(windowTitle, L"Chrome");
+    if (findP != nullptr) {
+        outputToConsole(windowTitle);
+        wchar_t format[64] = {};
+        wsprintf(format, L"%p", hwnd);
+        outputToConsole(format);
+        hwnds.push_back(hwnd);
+
+        SetWindowPos(hwnd, HWND_TOPMOST, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE);
+    }
+    else {
+        outputToConsole(L"not found ");
+    }
+    return true;
+}
+
+void outputToConsole(const wchar_t* text)
+{
+    HANDLE hStdOutput = GetStdHandle(STD_OUTPUT_HANDLE);
+
+    DWORD dwWriteByte;
+    TCHAR szBuf[256];
+    lstrcpy(szBuf, text);
+    WriteConsole(hStdOutput, szBuf, lstrlen(szBuf), &dwWriteByte, NULL);
+
+    TCHAR newline[256];
+    lstrcpy(newline, L"\n");
+    WriteConsole(hStdOutput, newline, lstrlen(newline), &dwWriteByte, NULL);
+}
 
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow)
 {
@@ -19,13 +64,15 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
 
     RegisterClass(&wc);
 
-    // Create the window.
+    AllocConsole();
+    AllowSetForegroundWindow(ASFW_ANY);
 
+    // Create the window.
     HWND hwnd = CreateWindowEx(
-        0,                              // Optional window styles.
+        WS_EX_TOPMOST | WS_EX_NOACTIVATE,            // Window style
         CLASS_NAME,                     // Window class
         L"Learn to Program Windows",    // Window text
-        WS_OVERLAPPEDWINDOW,            // Window style
+        0,                              // Optional window styles.
 
         // Size and position
         CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
@@ -36,12 +83,22 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
         NULL        // Additional application data
     );
 
+    BOOL res;
+    NOTIFYICONDATA notifyIconData;
+    notifyIconData.cbSize = sizeof(NOTIFYICONDATA);
+    notifyIconData.hWnd = hwnd;
+    notifyIconData.uID = 1001;
+    notifyIconData.uFlags = NIF_MESSAGE | NIF_ICON | NIF_TIP;
+    notifyIconData.uCallbackMessage = nullptr;
+    HICON hicon;
+    notifyIconData.hIcon = hicon;
+
     if (hwnd == NULL)
     {
         return 0;
     }
 
-    ShowWindow(hwnd, nCmdShow);
+    ShowWindow(hwnd, SW_SHOWNA);
 
     // Run the message loop.
 
@@ -60,6 +117,7 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
     switch (uMsg)
     {
     case WM_DESTROY:
+        FreeConsole();
         PostQuitMessage(0);
         return 0;
 
@@ -68,14 +126,16 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
         PAINTSTRUCT ps;
         HDC hdc = BeginPaint(hwnd, &ps);
 
-
-
         FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
 
         EndPaint(hwnd, &ps);
+        return 0;
     }
-    return 0;
-
+    case WM_CREATE:
+    {
+        EnumWindows(EnumWindowsProc, lParam);
+        return 0;
+    }
     }
     return DefWindowProc(hwnd, uMsg, wParam, lParam);
 }
