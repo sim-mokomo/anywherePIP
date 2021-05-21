0472d65becd61789e91006c24d94159315c6e42c
sim-mokomo <15795655+sim-mokomo@users.noreply.github.com>
2021-05-21 22:15:45 +0900

    fix: change app icon name

--- anywherePIP/MainForm.Designer.cs
+++ anywherePIP/MainForm.Designer.cs
@@ -44,7 +44,7 @@ namespace anywherePIP
             // 
             this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
             this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
-            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Text = "anywherePIP";
             this.notifyIcon1.Visible = true;
             this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon_MouseDoubleClick);
             // 
